0467770e6673e2333ffef161d14f20f0e2656035
sumitprasad002 <sumit02prasad@gmail.com>
2023-06-11 19:45:33 +0530

    Added react app structure

---
+++ twitter/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    //BEM convention
+    <div className="App">
+      <h1>Hey clever programmer welcome to React class</h1>
+    </div>
+  );
+}
+
+export default App;
